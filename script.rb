adf61f4861d028beec59cef21a470d9c8e5a5661
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2025-12-08 15:13:11 +0100

    Optimize code
    Use between? to check if a charactr in ascii is between two values
    User  % 26 (instead of another if) to define the shifted position in a way that
    works for all caracters

--- script.rb
+++ script.rb
@@ -1,49 +1,20 @@
-require 'pry-byebug'
- 
-# Project: Caesar Cipher
-
-# Pseuddo-Code:
-# worte in eine array setzen: 
-# 
-#Array in Ascii Code umwandeln
-#alle Buchstaben prüfen:
-#Falls in der range von kleinbuchstaben
-# kleinbuchstaben verschieben: 
-# falls buchstabe ascii < z ascii - number = verschieben (buchstabe + number)
-# ansonsten buchstabe am anfang veschieben = asci-a + (number - (asci z - buchstabe)) 
-
-
-def caesar_cipher(input_string, number)
-  kleinbuchstaben_start = "a".ord
-  kleinbuchstaben_ende = "z".ord
-
-  grossbuchstaben_start = "A".ord
-  grossbuchstaben_ende = "Z".ord
-  buchstaben = input_string.split
-  buchstaben = buchstaben.map{|wort| wort.chars.map{|buchstabe|buchstabe.ord}}
-  buchstaben = buchstaben.map do |wort|
-    wort.map do |buchstabe|
-      if buchstabe >= kleinbuchstaben_start && buchstabe <= kleinbuchstaben_ende
-        if buchstabe <= (kleinbuchstaben_ende -number)
-          buchstabe += number
-        else
-          buchstabe = kleinbuchstaben_start + (number - (kleinbuchstaben_ende - buchstabe)) -1
-        end
-      elsif buchstabe >= grossbuchstaben_start && buchstabe <= grossbuchstaben_ende
-        if buchstabe <= (grossbuchstaben_ende -number)
-          buchstabe += number
-        else
-          buchstabe = grossbuchstaben_start + (number - (grossbuchstaben_ende - buchstabe)) -1
-        end
-      else 
-        buchstabe
-      end
+def caesar_cipher2(input_string, shift)
+  shift %= 26
+  LOWER_CASE_START = "a".ord
+  LOWER_CASE_END = "z".ord
+  UPPER_CASE_START = "A".ord
+  UPPER_CASE_END = "Z".ord
+  output = ""
+  input_string.each_char do |char|
+    ascii_char = char.ord
+    if ascii_char.between?(LOWER_CASE_START, LOWER_CASE_END)
+      output += (((ascii_char - LOWER_CASE_START) + shift) % 26 + LOWER_CASE_START).chr
+    elsif ascii_char.between?(UPPER_CASE_START, UPPER_CASE_END)
+     output += (((ascii_char - UPPER_CASE_START) + shift) % 26 + UPPER_CASE_START).chr
+    else
+      output += ascii_char.chr
     end
-    
   end
-buchstaben = buchstaben.map { |wort| wort.map {|buchstabe|buchstabe.chr}.join}.join(" ")
-
+  output
 end
-
 caesar_cipher("What a string!", 5)
-
