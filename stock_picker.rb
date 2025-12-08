b9ebe0e166d6be4a0c89afc38741df7de37fa71c
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2025-12-08 22:55:35 +0100

    Change variable names for better readability

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,21 +1,21 @@
 def stock_picker(stock_prices)
   profits = Array.new
-  i = 0
-  until i == stock_prices.length do
+  current_day = 0
+  until current_day == stock_prices.length do
     profits.push({
       profit: 0,
-      buy_day: i,
+      buy_day: current_day,
       sell_day: nil,
     })    
-      j = i+1
-      until j == stock_prices.length do
-        if ((stock_prices[j] - stock_prices[i]) > profits.last[:profit])
-          profits.last[:profit] = stock_prices[j] - stock_prices[i] 
-          profits.last[:sell_day] = j 
+      subsequent_day = current_day + 1
+      until subsequent_day == stock_prices.length do
+        if ((stock_prices[subsequent_day] - stock_prices[current_day]) > profits.last[:profit])
+          profits.last[:profit] = stock_prices[subsequent_day] - stock_prices[current_day] 
+          profits.last[:sell_day] = subsequent_day 
         end
-        j += 1
+        subsequent_day += 1
       end 
-    i += 1
+    current_day += 1
   end
   best_day =  profits.max_by do |day|
     day[:profit]
