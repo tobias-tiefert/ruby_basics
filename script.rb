f6fd825d3eaf81ed23a4501d68ca75522ded0c9c
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2025-12-08 15:16:11 +0100

    Add comments

--- script.rb
+++ script.rb
@@ -1,4 +1,4 @@
-def caesar_cipher2(input_string, shift)
+def caesar_cipher(input_string, shift)
   shift %= 26
   LOWER_CASE_START = "a".ord
   LOWER_CASE_END = "z".ord
@@ -8,6 +8,7 @@ def caesar_cipher2(input_string, shift)
   input_string.each_char do |char|
     ascii_char = char.ord
     if ascii_char.between?(LOWER_CASE_START, LOWER_CASE_END)
+      # The % 26 ensures, that the definition works if the shift goes higher than z
       output += (((ascii_char - LOWER_CASE_START) + shift) % 26 + LOWER_CASE_START).chr
     elsif ascii_char.between?(UPPER_CASE_START, UPPER_CASE_END)
      output += (((ascii_char - UPPER_CASE_START) + shift) % 26 + UPPER_CASE_START).chr
